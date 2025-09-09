eb587abae60c988d0fb1b3f8f03cc9c49414b9f4
Murilo <m13sumikawa@gmail.com>
2025-09-09 19:45:53 -0300

    Initial commit

---
+++ opcoes.js
@@ -0,0 +1,52 @@
+    const raçasB = [
+      "Humano", "Anão", "Dahllan", "Elfo", "Goblin", "Lefou", "Minotauro", "Qareen", "Golem de ferro", "Hynne", "Kliren", "Medusa", "Osteon", "Sereia/Tritão", "Sílfide", "Aggelus", "Sulfure", "Trog", "Bugbear", "Centauro", "Ceratops", "Elfo-do-mar", "Finntroll", "Gnoll", "Harpia", "Hobgoblin", "Kallyanach", "Kaijin", "Kappa", "Kobolds", "Meio-orc", "Minauro", "Moreau da coruja", "Moreau da hiena", "Moreau da raposa", "Moreau da serpente", "Moreau do búfalo", "Moreau do coelho", "Moreau do crocodilo", "Moreau do gato", "Moreau do leão", "Moreau do lobo", "Moreau do morcego", "Moreau do Urso", "Nagah", "Nezumi", "Ogro", "Orc", "Pteros", "Tabrachi", "Tengu", "Velocis", "Voracis", "Yidishan", "Duende", "Eiradaan", "Galokk", "Meio-Elfo", "Sátiro"
+    ];
+
+    const raçasV = [
+      "Golem de barro", "Golem de bronze", "Golem de carne", "Golem de espelhos", "Golem de gelo eterno", "Golem de pedra", "Golem de sucata", "Osteon soterrado", "Trog anão", "Aggelus de Al-Gazara", "Aggelus de Arbória", "Aggelus de Chacina", "Aggelus de Deathok", "Aggelus de Drashantyr", "Aggelus de Kundali", "Aggelus de Magika", "Aggelus de Nivenciuén", "Aggelus de Odisseia", "Aggelus de Ordine", "Aggelus de Pelágia", "Aggelus de Pyra", "Aggelus de Ramknal", "Aggelus de Serena", "Aggelus de Skerry", "Aggelus de Solaris", "Aggelus de Sombria", "Aggelus de Sora", "Aggelus de Terápolis", "Aggelus de Venomia", "Aggelus de Vitalia", "Aggelus de Werra", "Sulfure de Al-Gazara", "Sulfure de Arbória", "Sulfure de Chacina", "Sulfure de Deathok", "Sulfure de Drashantyr", "Sulfure de Kundali", "Sulfure de Magika", "Sulfure de Nivenciuén", "Sulfure de Odisseia", "Sulfure de Ordine", "Sulfure de Pelágia", "Sulfure de Pyra", "Sulfure de Ramknal", "Sulfure de Serena", "Sulfure de Skerry", "Sulfure de Solaris", "Sulfure de Sombria", "Sulfure de Sora", "Sulfure de Terápolis", "Sulfure de Venomia", "Sulfure de Vitalia", "Sulfure de Werra"
+    ];
+
+    const classesB = [
+      "Arcanista bruxo", "Arcanista feiticeiro dracônico", "Arcanista feiticeiro feérico", "Arcanista feiticeiro rubro", "Arcanista mago", "Bárbaro", "Bardo", "Bucaneiro", "Caçador", "Cavaleiro", "Clérigo", "Druida", "Guerreiro", "Inventor", "Ladino", "Lutador", "Nobre", "Paladino", "Treinador", "Frade", "Arcanista feiticeiro abençoado"
+    ];
+
+    const classesV = [
+      "Necromante", "Machado de Pedra", "Magimarcialista", "Duelista", "Seteiro", "Vassalo", "Usurpador", "Ermitão", "Inovador", "Alquimista", "Ventanista", "Atleta", "Burguês", "Santo"
+    ];
+    
+    const origensB = [
+      "Acólito", "Amigo dos Animais", "Amnésico", "Aristocrata", "Artesão", "Artista", "Assistente de Laboratório", "Batedor", "Capanga", "Charlatão", "Circense", "Criminoso", "Curandeiro", "Eremita", "Escravo", "Estudioso", "Fazendeiro", "Forasteiro", "Gladiador", "Guarda", "Herdeiro", "Herói Camponês", "Marujo", "Mateiro", "Membro de Guilda", "Mercador", "Minerador", "Nômade", "Pivete", "Refugiado", "Seguidor", "Selvagem", "Soldado", "Taverneiro", "Trabalhador"
+    ];
+
+    const origensE  = [
+      "Agricultor Sambur (Atlas)","Amazona de Hippion (Atlas)","Amoque Púrpura (Atlas)","Anão de Armas (Atlas)","Andarilho Ubaneri (Atlas)","Aprendiz de Dragoeiro (Atlas)","Aprendiz de Drogadora (Atlas)","Aristocrata Dai’zenshi (Atlas)","Armeiro Armado (Atlas)","Aspirante a herói (Atlas)","Assistente Forense (Atlas)","Bandoleiro da Fortaleza (Atlas)","Barão Arruinado (Atlas)","Catador da Cidade Velha (Atlas)","Cativo das Fadas (Atlas)","Competidor do Circuito (Atlas)","Cosmopolita (Atlas)","Cria da Favela (Atlas)","Criado pelas Voracis (Atlas)","De Outro Mundo (Atlas)","Descendente colleniano (Atlas)","Desertor da Supremacia (Atlas)","Duplo Feérico (Atlas)","Duyshidakk Infiltrado (Atlas)","Emissário Ubaneri (Atlas)","Escudeiro da Luz (Atlas)","Escudeiro Solitário (Atlas)","Estandarte Vivo (Atlas)","Estudante da Academia (Atlas)","Estudante do Colégio Real (Atlas)","Explorador de Ruínas (Atlas)","Filhote da Revoada (Atlas)","Futura Lenda (Atlas)","Ginete de Tumarkhân (Atlas)","Grumete Pirata (Atlas)","Guardião Glacial (Atlas)","Iniciado dos Caça-Monstros (Atlas)","Insurgente Tapistano (Atlas)","Irmão Sem Esporas (Atlas)","Legionário (Atlas)","Lenhador de Tollon (Atlas)","Liricista de Lenórienn (Atlas)","Membro do Principado (Atlas)","Nitamuraniano (Atlas)","Nobre Zakharoviano (Atlas)","Nômade Sar-Allan (Atlas)","Pescador Parrudo (Atlas)","Plebeu Arcano (Atlas)","Prisioneiro das Catacumbas (Atlas)","Procurado: Vivo ou Morto (Atlas)","Profeta do Akzath (Atlas)","Querido Filho (Atlas)","Rebelde Agitador (Atlas)","Receptador das Nuvens (Atlas)","Recruta Arcano (Atlas)","Recruta da Fênix (Atlas)","Sábio Matemático (Atlas)","Selvagem Sanguinário (Atlas)","Sucateiro de Batalhas (Atlas)","Tamalu (Atlas)","Tocado pela Dama Altiva (Atlas)","Tocado pelo Indomável (Atlas)","Tradicionalista Svalano (Atlas)","Trapaceiro Ahleniense (Atlas)","Turista da Academia (Atlas)","Um com os Kami (Atlas)","Bacharel (Heróis)","Boticário (Heróis)","Caçador de Ratos (Heróis)","Cão de Briga (Heróis)","Carcereiro (Heróis)","Carpinteiro de Guilda (Heróis)","Catador da Catástrofe (Heróis)","Chef Hynne (Heróis)","Cirurgião-Barbeiro (Heróis)","Citadino Abastado (Heróis)","Cocheiro (Heróis)","Construtor (Heróis)","Contrabandista (Heróis)","Coureiro (Heróis)","Escriba (Heróis)","Espião (Heróis)","Ferreiro Militar (Heróis)","Freira (Heróis)","Goradista (Heróis)","Insciente (Heróis)","Interrogador (Heróis)","Ladrão de Túmulos (Heróis)","Menestrel (Heróis)","Mensageiro (Heróis)","Náufrago (Heróis)","Padeiro (Heróis)","Pedinte (Heróis)","Pescador (Heróis)","Servo (Heróis)","Suporte de Tropas (Heróis)"
+    ];
+
+    const devoçõesB = [
+      "Nenhuma", "Aharadak", "Allihanna", "Arsenal", "Azgher", "Hyninn", "Kallyadranoch", "Khalmyr", "Lena", "Lin-Wu", "Marah", "Megalokk", "Nimb", "Oceano", "Sszzaas", "Tanna-Toh", "Tenebra", "Thwor", "Thyatis", "Valkaria", "Wynna"
+    ];
+
+    const deusesM = [
+      "Gwendolynn", "Mauziell", "Tibar", "A Espada-Deus", "Akok", "Altair", "Anilatir", "Apis", "Artaphan", "Ayllana", "Beluhga ", "Benthos", "Betsumial", "Blinar", "Caerdellach", "Canastra", "Canora", "Cette", "Champarr", "Dahriol", "Drumak", "Dunsark", "Elrophin", "Escamandra", "Esmeralda", "Garanaam", "Garth", "Goharom", "Granto", "Gratissa ", "Hippion", "Hurlaagh", "Hydora", "Inghlblhpholstgt", "Irione", "Jandra", "Klangor", "Kurur Lianth", "Laan", "Lamashtu", "Lupan", "Luvithy", "Marina", "Mzzileyn", "Nerelim", "Neruíte", "O Deus Cristal de Urielka", "O Deus das Cidades", "O Deus do Medo", "Piscigeros", "Rhond", "Sckhar", "Sunnary", "Tamagrah", "Teldiskan", "Tessalus", "Toris", "Tukala", "Ur", "Yasshara", "Zadbblein", "Zakharov"
+    ];
+
+    function sortear(lista) {
+      return lista[Math.floor(Math.random() * lista.length)];
+    }
+
+    function criar() {
+      const incluirRaçaV = document.getElementById("raçaV").checked;
+      const incluirClasseV = document.getElementById("classeV").checked;
+      const incluirDeusesM = document.getElementById("deusesM").checked;
+      const incluirOrigemE = document.getElementById("origemE").checked;
+
+      const raças = incluirRaçaV ? [...raçasB, ...raçasV] : raçasB;
+      const classes = incluirClasseV ? [...classesB, ...classesV] : classesB;
+      const devoções = incluirDeusesM ? [...devoçõesB, ...deusesM] : devoçõesB;
+      const origens = incluirOrigemE ? [...origensB, ...origensE] : origensB;
+
+      document.getElementById("raça").textContent = sortear(raças);
+      document.getElementById("origem").textContent = sortear(origens);
+      document.getElementById("classe").textContent = sortear(classes);
+      document.getElementById("devoção").textContent = sortear(devoções);
+    }
